3f73abadb18110b3650f583499aebbf1f058d0c8
Amith Sahani <95488369+amith1315@users.noreply.github.com>
2025-07-08 13:51:25 +0530

    Add files via upload

---
+++ keylogger/src/ui/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className='mt-32'>
+      <h1 className='text-4xl font-mono'>Key Gaurdian</h1>
+    </div>
+  )
+}
+
+export default App
